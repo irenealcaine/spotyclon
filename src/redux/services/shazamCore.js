c7025a937dacd929c3fdd9d8a1e2a7251a85d98b
Irene Alcaine <irenealcainealvarez@gmail.com>
2022-10-24 21:48:27 +0200

    trying env to work

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,11 +1,15 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+const KEY = import.meta.env.VITE_SHAZAM_CORE_RAPID_API_KEY;
+
 export const shazamCoreApi = createApi({
   reducerPath: 'shazamCoreApi',
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
     prepareHeaders: (headers) => {
-      headers.set('X-RapidAPI-Key', import.meta.env.VITE_SHAZAM_CORE_RAPID_API_KEY);
+      headers.set('X-RapidAPI-Key', KEY);
+      // eslint-disable-next-line no-console
+      console.log(KEY);
       return headers;
     },
   }),
