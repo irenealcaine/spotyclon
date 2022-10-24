03bfbc99a87d01649e109745b7c9b18bab069243
Irene Alcaine <irenealcainealvarez@gmail.com>
2022-10-24 18:55:47 +0200

    env

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -14,7 +14,7 @@ const Searchbar = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit} autoComplete="off" className="p-2 text-gray-400 focus-Within:-text-gray-600">
+    <form onSubmit={handleSubmit} autoComplete="off" className="p-2 text-gray-300 focus-Within:-text-gray-600">
       <label htmlFor="search-field" className="sr-only">Serach all songs</label>
       <div className="flex flex-row justify-start items-center">
         <FiSearch className="w-5 h-5 ml-4" />
