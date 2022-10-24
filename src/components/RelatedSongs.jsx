6179cb914b571d4698e243d06158d8b02c25ca2e
Irene Alcaine <irenealcainealvarez@gmail.com>
2022-10-24 18:43:29 +0200

    first commit

---
+++ src/components/RelatedSongs.jsx
@@ -0,0 +1,24 @@
+import SongBar from './SongBar';
+
+const RelatedSongs = ({ data, handlePauseClick, handlePlayClick, isPlaying, activeSong, artistId }) => (
+  <div className="flex flex-col">
+    <h1 className="font-bold text-white text-3xl">Related songs:</h1>
+    <div className="mt-6 w-full flex flex-col">
+      {data?.map((song, i) => (
+        <SongBar
+          key={`${song.key}-${artistId}`}
+          song={song}
+          i={i}
+          artistId={artistId}
+          isPlaying={isPlaying}
+          activeSong={activeSong}
+          handlePauseClick={handlePauseClick}
+          handlePlayClick={handlePlayClick}
+
+        />
+      ))}
+    </div>
+  </div>
+);
+
+export default RelatedSongs;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { RiCloseLine } from 'react-icons/ri';
+
+import { HiOutlineMenu } from 'react-icons/hi';
+import melody from '../assets/melody.svg';
+import { links } from '../assets/constants';
+
+const NavLinks = ({ handleClick }) => (
+  <div className="mt-10">
+    {links.map((item) => (
+      <NavLink
+        key={item.name}
+        to={item.to}
+        className="flex flex-row justify-start items-center my-8 text-sm font-medium text-gray-400 hover:text-cyan-400"
+        onClick={() => handleClick && handleClick()}
+      >
+        <item.icon className="w-6 h-6 mx-2" />
+        {item.name}
+      </NavLink>
+    ))}
+  </div>
+);
+
+const Sidebar = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  return (
+    <>
+      <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
+        <img src={melody} alt="logo" className="w-full h-24 object-contain" />
+        <p className="uppercase text-cyan-400 w-full text-center font-bold text-3xl">Spotyclon</p>
+        <NavLinks />
+      </div>
+
+      <div className="absolute md:hidden block top-6 right-3 z-10">
+        {mobileMenuOpen ? (
+          <RiCloseLine className="w-6 h-6 text-white mr-2" onClick={() => setMobileMenuOpen(false)} />
+        ) : (
+          <HiOutlineMenu className="w-6 h-6 text-white mr-2" onClick={() => setMobileMenuOpen(true)} />
+        )}
+      </div>
+
+      <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#483d8d] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0' : '-left-full'}`}>
+        <img src={melody} alt="logo" className="w-full h-24 object-contain" />
+        <p className="uppercase text-cyan-400 w-full text-center font-bold text-3xl">Spotyclon</p>
+
+        <NavLinks handleClic={() => setMobileMenuOpen(false)} />
+      </div>
+    </>
+  );
+};
+
+export default Sidebar;
