311b4ea725657bf28bc9ebfcaaa8738923b7cd2f
Irene Alcaine <irenealcainealvarez@gmail.com>
2022-12-16 21:15:53 +0100

    change player bars color

--- src/components/MusicPlayer/Seekbar.jsx
+++ src/components/MusicPlayer/Seekbar.jsx
@@ -17,7 +17,7 @@ const Seekbar = ({ value, min, max, onInput, setSeekTime, appTime }) => {
         min={min}
         max={max}
         onInput={onInput}
-        className="md:block w-24 md:w-56 2xl:w-96 h-1 mx-4 2xl:mx-6 rounded-lg"
+        className="md:block w-24 md:w-56 2xl:w-96 h-1 mx-4 2xl:mx-6 rounded-lg accent-green-700"
       />
       <p className="text-white">{max === 0 ? '0:00' : getTime(max)}</p>
       <button type="button" onClick={() => setSeekTime(appTime + 5)} className="hidden lg:ml-4 lg:block text-white">
